8ad9cfe4208db587dbea57945585d29f0cda4489
redchchicken75 <tanhongjie.cs07@nctu.edu.tw>
2022-07-19 17:09:38 +0800

    Frontpage updated

--- js/countdown.js
+++ js/countdown.js
@@ -4,11 +4,6 @@ const countDownTime = new Date(
   clockdiv.getAttribute("data-date")
 ).getTime();
 
-// console.log(countDownTime);
-const currentDate = new Date();
-const date2 = new Date(countDownTime);
-console.log(currentDate.toString());
-console.log(date2.toString());
 if(countDownTime <= Date.now()) {
   console.log("It's time");
   window.location = "pages/frontpage.html";
@@ -19,20 +14,25 @@ if(countDownTime <= Date.now()) {
 // Set interval function used to call the function at every interval
 // The function if called every 1000ms 
 const countdownfunction = setInterval(function () {
-  const now = new Date().getTime();
-  const diff = countDownTime - now;
-  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-  const hours = Math.floor(diff % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
-  const minutes = Math.floor(diff % (1000 * 60 * 60) / (1000 * 60));
-  const seconds = Math.floor(diff % (1000 * 60) / 1000);
-
-  if (diff < 0) {
-    clockdiv.style.display = "none";
-    clearInterval(countdownfunction);
+  if(countDownTime <= Date.now()) {
+    window.location = "pages/frontpage.html";
   } else {
-    clockdiv.querySelector(".days").innerHTML = days;
-    clockdiv.querySelector(".hours").innerHTML = hours;
-    clockdiv.querySelector(".minutes").innerHTML = minutes;
-    clockdiv.querySelector(".seconds").innerHTML = seconds;
+    const now = new Date().getTime();
+    const diff = countDownTime - now;
+    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(diff % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+    const minutes = Math.floor(diff % (1000 * 60 * 60) / (1000 * 60));
+    const seconds = Math.floor(diff % (1000 * 60) / 1000);
+
+    if (diff < 0) {
+      clockdiv.style.display = "none";
+      clearInterval(countdownfunction);
+    } else {
+      clockdiv.querySelector(".days").innerHTML = days;
+      clockdiv.querySelector(".hours").innerHTML = hours;
+      clockdiv.querySelector(".minutes").innerHTML = minutes;
+      clockdiv.querySelector(".seconds").innerHTML = seconds;
+    }
   }
+  
 }, 1000);
